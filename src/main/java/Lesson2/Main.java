05214ef6d77e3df9e051d9410d1f84cdd9edee19
vedmas <07011984t@mail.ru>
2019-08-12 18:36:38 +0500

    Сделал несколько массивов для сортировки

--- src/main/java/Lesson2/Main.java
+++ src/main/java/Lesson2/Main.java
@@ -1,45 +1,55 @@
 package Lesson2;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
         long start, finish, timeConsumedMillis;
-        Array<Integer> array = new ArrayOperation<>(5);
+        Array<Integer> arrayBubble = new ArrayOperation<>(5);
 //        Array<Integer> array = new SortedArrayOperation<>();
-        for (int i = 0; i < 1000000; i++) {
-            array.add(array.createValue());
+        for (int i = 0; i < 10000; i++) {
+            arrayBubble.add(arrayBubble.createValue());
         }
 
+        Array<Integer> arraySelect = new ArrayOperation<>(5);
+        for (int i = 0; i < 10000; i++) {
+            arraySelect.add(arrayBubble.createValue());
+        }
+
+        Array<Integer> arrayInsert = new ArrayOperation<>(5);
+        for (int i = 0; i < 10000; i++) {
+            arrayInsert.add(arrayBubble.createValue());
+        }
+
+
+
         start = System.currentTimeMillis();
-        array.sortBubble();
+        arrayBubble.sortBubble();
         finish = System.currentTimeMillis();
         System.out.println(("Time consumed to sort by sortBubble method: " + (finish - start) + " millis"));
 
         start = System.currentTimeMillis();
-        array.sortSelect();
+        arraySelect.sortSelect();
         finish = System.currentTimeMillis();
         System.out.println(("Time consumed to sort by sortSelect method: " + (finish - start) + " millis"));
 
         start = System.currentTimeMillis();
-        array.sortInsert();
+        arrayInsert.sortInsert();
         finish = System.currentTimeMillis();
         System.out.println(("Time consumed to sort by sortInsert method: " + (finish - start) + " millis"));
 
-        //array.sortBubble();
-        //array.sortSelect();
-
-
 //        array.display();
 //
-        System.out.println("Remove: " + array.remove(Integer.valueOf(2)));
-        System.out.println("Remove: " + array.remove(Integer.valueOf(200)));
+        System.out.println("Remove: " + arrayBubble.remove(Integer.valueOf(2)));
+        System.out.println("Remove: " + arrayBubble.remove(Integer.valueOf(200)));
 
 //        array.display();
 
-        System.out.println("Find: " + array.indexOf(1));
-        System.out.println("Find: " + array.indexOf(7));
-        System.out.println("Find: " + array.indexOf(5));
-        System.out.println("Find: " + array.indexOf(8));
-        array.display();
+        System.out.println("Find: " + arrayBubble.indexOf(1));
+        System.out.println("Find: " + arrayBubble.indexOf(7));
+        System.out.println("Find: " + arrayBubble.indexOf(5));
+        System.out.println("Find: " + arrayBubble.indexOf(8));
+        arraySelect.display();
 
 
     }
