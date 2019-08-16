0f34f23fd4b0660c6b1d1c627cda6dd608a9df70
vedmas <07011984t@mail.ru>
2019-08-16 15:22:36 +0500

    Добавил тест на очередь

--- src/test/java/TestMethods.java
+++ src/test/java/TestMethods.java
@@ -1,15 +1,16 @@
+import DataStructures.Lesson3.Queue.QueueImpl;
 import DataStructures.Lesson3.Stack.StackImpl;
 import org.junit.*;
 
 public class TestMethods {
     private StackImpl stack;
+    private QueueImpl queue;
 
     @Before
     public void testStack() {
         stack = new StackImpl(5);
         stack.push(2);
         stack.push(3);
-
     }
 
     @org.junit.Test
@@ -20,6 +21,14 @@ public class TestMethods {
     @Test
     public void testPop() {
         Assert.assertEquals(3, stack.pop());
+    }
+
+    @Test
+    public void testInsert() {
+        queue = new QueueImpl(5);
+        queue.insert(2);
+        queue.insert(3);
+        Assert.assertEquals(2, queue.size());
     }
 
 
